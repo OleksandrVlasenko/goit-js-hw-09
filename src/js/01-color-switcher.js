3796b59fd22c435551dae2d6bd12c20df2300ab4
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-03-18 23:49:10 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+stopBtn.setAttribute('disabled', 'disabled');
+
+startBtn.addEventListener('click', startSwitchCollor);
+stopBtn.addEventListener('click', stopSwitchCollor);
+
+function startSwitchCollor() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+}
+
+function stopSwitchCollor() {
+  clearInterval(timerId);
+
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
